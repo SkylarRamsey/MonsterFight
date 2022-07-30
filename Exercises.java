c3e7c7b78d2621d09912f16859c0a48189099d1b
Skylar Ramsey <skylarramsey362@yahoo.com>
2022-07-29 22:52:20 -0400

    Added Exercises class

---
+++ Exercises.java
@@ -0,0 +1,176 @@
+
+public class Exercises {
+
+	String muscleGroup = null;
+	
+	public Exercises(String muscleGroup)
+	{
+		this.muscleGroup = muscleGroup;
+	}
+	
+	public selectExercises(int entry)
+	{
+		if(this.muscleGroup == "Arms")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+		if(this.muscleGroup == "Back")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+		if(this.muscleGroup == "Legs")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+		if(this.muscleGroup == "Core")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+		if(this.muscleGroup == "Mobility")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+		if(this.muscleGroup == "Cardio")
+		{
+			do
+			{
+				switch(entry)
+				{
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						break;
+						
+									
+					default:
+						break;
+						
+							
+				}
+			}
+			while(entry != 5);
+		}
+		
+	}
+}
